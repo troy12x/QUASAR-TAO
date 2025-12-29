f2e0631d69e3ade7b2fb2d4f1bb103c86da44ab6
Eyad Gomaa <61633360+troy12x@users.noreply.github.com>
2025-12-30 00:57:04 +0200

    Supabase

--- validator_api/database.py
+++ validator_api/database.py
@@ -6,7 +6,9 @@ import os
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./quasar_validator.db")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+    SQLALCHEMY_DATABASE_URL, 
+    pool_pre_ping=True,
+    connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
